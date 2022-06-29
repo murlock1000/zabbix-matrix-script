5be997ad8b466735624b541464fd58282275adec
murlock1000 <pauliusjankauskas10@gmail.com>
2022-06-29 15:55:42 +0300

    Add docker launch script

--- launch.sh
+++ launch.sh
@@ -5,6 +5,8 @@ to="$1"
 subject="$2"
 message="$3"
 
-docker run --mount type=bind,source="$(pwd)"/config.json,target=/usr/src/app/config.json --mount type=bind,source="$(pwd)"/store,target=/usr/src/app/store zabbixmatrixscript $to $subject $message
+workingDir="/usr/lib/zabbix/alertscripts/zabbix-matrix-script"
+
+docker run --mount type=bind,source="${workingDir}"/config.json,target=/usr/src/app/config.json --mount type=bind,source="${workingDir}"/store,target=/usr/src/app/store zabbixmatrixscript "${to}" "${subject}" "${message}"
 
 exit $?
